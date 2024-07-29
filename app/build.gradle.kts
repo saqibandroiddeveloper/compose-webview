e8d448f27412a7101b77f0ca93a6d5aa74f1586f
Muhammad Saqib Khan <m.saqib.personal007@gmail.com>
2024-07-29 23:11:46 +0500

    Merge pull request #1
    * issue solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("com.github.fengdai.compose:pulltorefresh:0.2.0")
+    implementation ("androidx.compose.material:material:1.6.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -69,4 +70,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
